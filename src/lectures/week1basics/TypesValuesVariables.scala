6e21e785786b05cc778dbb5cfa6b3a41d4d52b02
Svyatoslav <svyatyar2@gmail.com>
2021-10-22 22:56:30 +0300

    add: types, val, var

---
+++ src/lectures/week1basics/TypesValuesVariables.scala
@@ -0,0 +1,17 @@
+package lectures.week1basics
+
+object TypesValuesVariables extends App{
+  var aString = "Hello"
+  val aChar = 'C'
+
+  val aInt = 11
+  val aLong = 11L
+  val aFloat = 2.0f
+  val aDouble = 53.249
+
+  val aBoolean = true
+
+  aString = "Hello, world"
+
+  println(aDouble.getClass)
+}
