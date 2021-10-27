659634ef63418a8d558f0d976c85a68fb0bcb3a6
Svyatoslav <svyatyar2@gmail.com>
2021-10-27 20:43:02 +0300

    add: LinkedList

---
+++ src/exercises/LinkedList.scala
@@ -0,0 +1,48 @@
+package exercises
+
+object ff extends App{
+  println(new Log("m1", new Log("m2", new Log("m3", Empty))).all)
+
+}
+
+
+abstract class LogList {
+  def last: String
+  def previous: LogList
+  def isEmpty: Boolean
+  def all: String
+  def add(msq: String): LogList
+}
+
+class Log(head: String, tail: LogList) extends LogList {
+  override def last: String = head
+
+  override def previous: LogList = tail
+
+  override def isEmpty: Boolean = false
+
+  override def all: String = {
+    def loop(logList: LogList = this, acc: String = ""): String ={
+      if (logList.isEmpty) acc
+      else loop(logList.previous, s"$acc ${logList.last}")
+    }
+    loop().substring(1)
+  }
+
+  override def add(msq: String): LogList = new Log(msq,this)
+
+
+}
+
+object Empty extends LogList {
+  override def last: String = throw new NoSuchElementException
+
+  override def previous: LogList = throw new NoSuchElementException
+
+  override def isEmpty: Boolean = true
+
+  override def all: String = ""
+
+  override def add(msq: String): LogList = new Log(msq, Empty)
+}
+
--- src/exercises/LogSystem.scala
+++ src/exercises/LogSystem.scala
@@ -1,23 +1,16 @@
 package exercises
 
-object LogSystem extends App{
 
-  class Logger(val msqNum: Int = 0) {
-    def info : Logger = {
-      println("INFO New Message")
-      new Logger(msqNum + 1)
-    }
-
-    def info(n: Int): Logger = {
-      if (n <= 0) this
-      else info.info(n-1)
-    }
-
-    def print: Unit = println(msqNum)
+class Logger(val msqNum: Int = 0) {
+  def info : Logger = {
+    println("INFO New Message")
+    new Logger(msqNum + 1)
   }
 
-  val logger = new Logger
-  logger.info.info.info.info(6).print
-  logger.info.print
-}
+  def info(n: Int): Logger = {
+    if (n <= 0) this
+    else info.info(n-1)
+  }
 
+  def print: Unit = println(msqNum)
+}
